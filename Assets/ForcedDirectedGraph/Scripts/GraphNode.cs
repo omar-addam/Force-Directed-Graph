58179d19c0d317e395742422e7beb0160fbbe205
omaddam <omaddam@gmail.com>
2020-12-19 01:48:49 -0700

    Created the graph node script and attached to the prefab.

---
+++ Assets/ForcedDirectedGraph/Scripts/GraphNode.cs
@@ -0,0 +1,42 @@
+﻿using ForceDirectedGraph.DataStructure;
+using UnityEngine;
+
+namespace ForceDirectedGraph
+{
+    public class GraphNode : MonoBehaviour
+    {
+
+        #region Initialization
+
+        /// <summary>
+        /// Initializes the graph entity.
+        /// </summary>
+        /// <param name="node">The node being presented</param>
+        public void Initialize(Node node)
+        {
+            _Node = node;
+
+            // Set the color
+            GetComponent<SpriteRenderer>().color = node.Color;
+        }
+
+        #endregion
+
+        #region Fields/Properties
+
+        /// <summary>
+        /// The node being presented.
+        /// </summary>
+        [SerializeField]
+        [Tooltip("The node being presented.")]
+        private Node _Node;
+
+        /// <summary>
+        /// The node being presented.
+        /// </summary>
+        public Node Node { get { return _Node; } }
+
+        #endregion
+
+    }
+}
