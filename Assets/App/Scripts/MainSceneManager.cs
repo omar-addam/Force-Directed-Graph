df5a3a2e04e0e76a7e1af74d07951881f5faac54
omaddam <omaddam@gmail.com>
2020-12-20 00:12:02 -0700

    Referenced the graph in the scene manager.

--- Assets/App/Scripts/MainSceneManager.cs
+++ Assets/App/Scripts/MainSceneManager.cs
@@ -1,8 +1,20 @@
+using ForceDirectedGraph;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MainSceneManager : MonoBehaviour
 {
+
+    #region Fields/Properties
+
+    /// <summary>
+    /// The graph displaying the network.
+    /// </summary>
+    [SerializeField]
+    [Tooltip("The graph displaying the network.")]
+    private GraphManager Graph;
+
+    #endregion
 
 }
