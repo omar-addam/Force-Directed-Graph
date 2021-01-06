d6c0c7b447f983ab2e91ecfa7889001e9e1d471d
omaddam <omaddam@gmail.com>
2021-01-05 23:08:51 -0700

    Got rid of the delay as it is not helping.

--- Assets/App/Scripts/MainSceneManager.cs
+++ Assets/App/Scripts/MainSceneManager.cs
@@ -18,8 +18,8 @@ public class MainSceneManager : MonoBehaviour
         // Display the app version
         DisplayVersion();
 
-        // Generate a sample after 3 seconds to have a stable framerate
-        Invoke(nameof(GenerateSample), 3);
+        // Generate a sample to visualize
+        GenerateSample();
     }
 
     #endregion
